8d4b32c2b6dd1d709ca74d8c9693a9a07d0a3b0e
Sergey Lebedkin <s.lebedkin@gmail.com>
2019-06-26 21:21:16 +0300

    resizable canvas added

--- src/com/ingrain/fabrics/ui/SelectWindowController.java
+++ src/com/ingrain/fabrics/ui/SelectWindowController.java
@@ -1,47 +1,93 @@
 package com.ingrain.fabrics.ui;
 
+import com.ingrain.fabrics.ui.controls.ResizableCanvas;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 // SelectWindowController
 public class SelectWindowController {
-	@FXML
-	private Button buttonSelectImage;
+    @FXML
+    private VBox mainPanel;
 
-	@FXML
-	private Canvas canvasMain;
+    @FXML
+    private Button buttonSelectImage;
+
+    @FXML
+    private Button buttonScaleDown;
+
+    @FXML
+    private Button buttonScaleUp;
+
+	// main canvas
+	private ResizableCanvas canvasMain;
+	private File file;
+	BufferedImage bufferedImage;
 
 	@FXML
 	void initialize() {
+		canvasMain = new ResizableCanvas();
+		canvasMain.setWidth(100);
+		canvasMain.setHeight(100);
+		canvasMain.setOnScroll(event -> {
+			if (event.getDeltaY() > 0) {
+				canvasMain.setWidth(canvasMain.getWidth()/1.3);
+				canvasMain.setHeight(canvasMain.getHeight()/1.3);
+				this.draw();
+			} else {
+				canvasMain.setWidth(canvasMain.getWidth()*1.3);
+				canvasMain.setHeight(canvasMain.getHeight()*1.3);
+				this.draw();
+			}
+		});
+		mainPanel.getChildren().add(canvasMain);
 	}
 
 	@FXML
 	void buttonSelectImageClick(MouseEvent event) throws IOException {
 		// create file chooser and load file
 		final FileChooser fileChooser = new FileChooser();
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Tiff files", "*.tif;*.tiff"));
-		File file = fileChooser.showOpenDialog(null);
-
-		// copy image to canvas
+		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Tiff files", "*.tif;*.tiff;*.jpg"));
+		file = fileChooser.showOpenDialog(null);
 		if (file != null) {
-			BufferedImage bufferedImage = ImageIO.read(file);
-			// create temporary writable image buffer for fast copy to canvas
+			bufferedImage = ImageIO.read(file);
+			draw();
+		}
+	}
+	
+	@FXML
+	void buttonScaleDownClick(MouseEvent event) throws IOException {
+		canvasMain.setWidth(canvasMain.getWidth()/2);
+		canvasMain.setHeight(canvasMain.getHeight()/2);
+		draw();
+	}
+	
+	@FXML
+	void buttonScaleUpClick(MouseEvent event) throws IOException {
+		canvasMain.setWidth(canvasMain.getWidth()*2);
+		canvasMain.setHeight(canvasMain.getHeight()*2);
+		draw();
+	}
+	
+	void draw() {
+		// copy image to canvas
+		if (bufferedImage != null) {
 			WritableImage writableImage = new WritableImage(bufferedImage.getWidth(), bufferedImage.getHeight());
 			// draw original image
-			canvasMain.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(bufferedImage, writableImage), 0, 0);
-			canvasMain.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(bufferedImage, writableImage), 100, 100);
+			canvasMain.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(bufferedImage, writableImage), 
+					0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(),
+					0, 0, canvasMain.getWidth(), canvasMain.getHeight());
 		}
 	}
 }
---
+++ src/com/ingrain/fabrics/ui/controls/ResizableCanvas.java
@@ -0,0 +1,10 @@
+package com.ingrain.fabrics.ui.controls;
+
+import javafx.scene.canvas.Canvas;
+
+public class ResizableCanvas extends Canvas {
+	@Override
+	public boolean isResizable() {
+		return true;
+	}
+}
